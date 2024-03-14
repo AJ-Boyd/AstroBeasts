a4a34f2d0751019bd01ddb3202a3bfc06a659173
AJ Boyd <alanjaredboyd3@yahoo.com>
2024-03-14 16:10:08 -0400

    updated route.py

--- astrobeasts/routes.py
+++ astrobeasts/routes.py
@@ -4,3 +4,26 @@ date: 2/26/24
 desc: this file hosts the different routes and endpoints in the flask application
 file: routes.py
 """
+
+from flask import Blueprint, render_template, request, jsonify
+import sqlite3
+
+router = Blueprint("router", __name__)
+
+
+@router.route("/", methods=["GET", "POST"])
+def start():
+    """
+    start()
+    will render the start screen template with the appropriate data
+    """
+    return render_template("start_screen.html")
+
+
+@router.route("/shop", methods=["GET", "POST"])
+def shop():
+    """
+    start()
+    will render the shop screen template with the appropriate data (shop's items)
+    """
+    return render_template("shop.html")
