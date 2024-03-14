791cbfddbcdf6e4ea12ea8d98874018d73281b32
AJ Boyd <alanjaredboyd3@yahoo.com>
2024-03-14 16:07:30 -0400

    set up init.py to actually run the app

--- astrobeasts/__init__.py
+++ astrobeasts/__init__.py
@@ -4,3 +4,16 @@ date: 2/26/24
 desc: initializes the databases and flask apps
 file: init.py
 """
+
+from flask import Flask
+import sqlite3
+
+
+def createApp():
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = "asdfasdfsadfSUPER-SECRET-keyyy!!!!"
+
+    from .routes import router
+
+    app.register_blueprint(router, url_prefix="/")
+    return app
