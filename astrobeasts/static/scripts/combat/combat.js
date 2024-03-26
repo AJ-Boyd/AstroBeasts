4bed8fe9bd2c2f34174204f958ff769e3529ccba
Casey <lo96097@umbc.edu>
2024-03-25 21:41:29 -0400

    Modifications to assets, added combat template initial steps

--- astrobeasts/static/scripts/combat/combat.js
+++ astrobeasts/static/scripts/combat/combat.js
@@ -1,1 +1,48 @@
-// export class (classnamehere) extends Phaser.Scene
+export class CombatScene extends Phaser.Scene {
+    constructor() {
+        super({
+            key:CombatScene.name,
+            active: true,
+        });
+        console.log(CombatScene.name);
+    }
+
+
+
+
+
+create() {
+
+    //background
+    console.log('create');
+
+    this.add.image(
+        this.scale.width/2,
+        this.scale.height/2, 
+        'background');
+
+//create enemy alien
+
+ this.add.sprite(600, 370, 'Icell').setScale(3);
+
+//create our alien
+
+ this.add.sprite(200, 370, 'Tyboar').setScale(4);
+
+
+
+
+}
+
+
+update() {
+
+console.log('update');
+
+
+
+
+}
+
+
+}
