51149d9292bf378a6733252115423f41d31aa06f
Josh Joo <159055924+jjoo3@users.noreply.github.com>
2024-03-29 05:20:22 -0400

    updated main.js to include all the new js files

--- astrobeasts/static/scripts/main.js
+++ astrobeasts/static/scripts/main.js
@@ -4,19 +4,25 @@ import { LoadGameScene } from './start_menu/loadgame.js';
 import { OptionsScene } from './start_menu/options.js';
 import { Preload } from './combat/preload.js';
 import { CombatScene }  from './combat/combat.js';
+import { HubScene } from './hub_menu/hub.js';
+import { InventoryScene } from './hub_menu/inventory.js';
+import { DojoScene } from './hub_menu/dojo.js';
+import { ShopScene } from './hub_menu/shop.js';
+import { TourneyScene } from './hub_menu/tourney.js';
+import { FleeScene } from './combat_menu/flee.js';
 
 // game configs
 const config = {
     type: Phaser.CANVAS,
     pixelArt: true,
-    scale: { 
-       
+    scale: {     
         width: 800,
         height: 600,
         mode: Phaser.Scale.FIT
     },
      parent: 'gameContainer',
-    scene: [MainMenuScene, NewGameScene, LoadGameScene, OptionsScene, Preload, CombatScene],
+    scene: [MainMenuScene, NewGameScene, LoadGameScene, OptionsScene, 
+            HubScene, InventoryScene, DojoScene, ShopScene, TourneyScene, FleeScene],       //Preload,CombatScene 
        dom: {
         createContainer: true
     },
@@ -32,4 +38,4 @@ const config = {
 const game = new Phaser.Game(config);
 
 //If loading directly to combat, uncomment the following line:
-game.scene.start('Preload');
+// game.scene.start('Preload');
