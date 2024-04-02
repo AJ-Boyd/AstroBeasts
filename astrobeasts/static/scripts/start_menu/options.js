8fc84189f20412e5c80db0e2e9e692e4722f59ab
Josh Joo <159055924+jjoo3@users.noreply.github.com>
2024-04-02 15:40:52 -0400

    Add files via upload

--- astrobeasts/static/scripts/start_menu/options.js
+++ astrobeasts/static/scripts/start_menu/options.js
@@ -5,10 +5,22 @@ export class OptionsScene extends Phaser.Scene {
     }
 
     create() {
-        this.add.text(100, 100, 'OPTIONS STUFF HERE', { color: '#fff' });
+        this.add.text(100, 100, 'OPTIONS STUFF HERE', { color: 'white' });
         // add options functionality here
+        
+        let HighScore = this.add.text(100, 200, 'High Scores', { color: 'magenta' })
+            .setInteractive({ useHandCursor: true })
+            .on('pointerdown', () => this.scene.start('HighScore'));
 
-        let GoBackText = this.add.text(100, 300, '< Back', { color: 'white' })
+        let smthn = this.add.text(100, 300, 'option 2', { color: 'magenta' })
+            .setInteractive({ useHandCursor: true })
+            .on('pointerdown', () => this.scene.start('HighScore'));
+
+        let smelse = this.add.text(100, 400, 'option 3', { color: 'deeppink' })
+            .setInteractive({ useHandCursor: true })
+            .on('pointerdown', () => this.scene.start('HighScore'));
+
+        let GoBackText = this.add.text(100, 500, '< Back', { color: 'white' })
             .setInteractive({ useHandCursor: true })
             .on('pointerdown', () => this.scene.start('MainMenu'));
     }
