63afd52eec0a0b340ead8d4ddc5639c54ec67cc6
Josh Joo <159055924+jjoo3@users.noreply.github.com>
2024-04-11 15:49:59 -0400

    added shop items

--- astrobeasts/static/scripts/start_menu/mainmenu.js
+++ astrobeasts/static/scripts/start_menu/mainmenu.js
@@ -5,30 +5,56 @@ export class MainMenuScene extends Phaser.Scene {
         super('MainMenu');
     }
     init() {
-        // inventory contains dictionary items, where each dictionary represents an item. it has the following variables:
-       // 1. key (image name), 2. name (item's name), 3. description, 4. quantity, 5. isEquipped (has the player equipped this item for battle?)
-       this.registry.set('inventory_items', []); // should be list of dicts with the item name and its key that cooresponds to it's png path
+        this.registry.set('playerName', '');
+        // Inventory contains dictionary items, where each dictionary represents an item. it has the following variables:
+        // 1. key (image name), 2. name (item's name), 3. description, 4. quantity, 5. isEquipped (has the player equipped this item for battle?)
+        this.registry.set('inventory_items', []); // should be list of dicts with the item name and its key that cooresponds to it's png path
        
-       // astrobeasts contain dictionaries, where each dictionary represents an astrobeast. it has the following variables:
-       // 1. key (image name), 2. name (astrobeast name), 3. description (it's affinity), 4. quantity (always 1), 5. isEquipped (has the player equipped this beast for battle?)
-       this.registry.set('inventory_astrobeasts', [{ key: 'skol', name: 'Skol', description: 'Fire AstroBeast', quantity: 1, cost: 50, isEquipped: false },
-       { key: 'tarkeel', name: 'Tarkeel', description: 'Water AstroBeast', quantity: 1, cost: 51, isEquipped: false }]); // similar as above. showing starter astrobeasts
+        // Astrobeasts contain dictionaries, where each dictionary represents an astrobeast. it has the following variables:
+        // 1. key (image name), 2. name (astrobeast name), 3. description (it's affinity), 4. quantity (always 1), 5. isEquipped (has the player equipped this beast for battle?)
+        this.registry.set('inventory_astrobeasts', [{ key: 'skol', name: 'Skol', description: 'Fire AstroBeast', quantity: 1, cost: 50, isEquipped: false },
+        { key: 'tarkeel', name: 'Tarkeel', description: 'Water AstroBeast', quantity: 1, cost: 51, isEquipped: false }]); // similar as above. showing starter astrobeasts
        
-
-       // moves contain dictionaries, where each dictionary represents a move. it has the following variables:
-       // 1. key (image name), 2. name (move name), 3. description (what it does), 4. quantity, 5. isEquipped (has the player equipped this  for battle?)
-       this.registry.set('inventory_moves', [{ key: 'slash', name: 'Slash', description: 'Deals damage to enemy', quantity: 2, cost: 30, isEquipped: false },
-       { key: 'headbutt', name: 'Headbutt', description: 'Deals damage to enemy', quantity: 25, isEquipped: false }]); 
+        // Moves contain dictionaries, where each dictionary represents a move. it has the following variables:
+        // 1. key (image name), 2. name (move name), 3. description (what it does), 4. quantity, 5. isEquipped (has the player equipped this  for battle?)
+        this.registry.set('inventory_moves', [
+        { key: 'slash', name: 'Slash', description: 'Deals damage to enemy', quantity: 1, cost: 30, isEquipped: false },
+        { key: 'headbutt', name: 'Headbutt', description: 'Deals damage to enemy', quantity: 1, cost:25, isEquipped: false }]); 
        
-       this.registry.set('shop_moves', [{ key: 'slash', name: 'Slash', description: 'Deals damage to enemy', quantity: 2, cost: 30, isEquipped: false },
-       { key: 'headbutt', name: 'Headbutt', description: 'Deals damage to enemy', quantity: 25, isSelected: false }]); 
-       this.registry.set('playerName', '');
+       
+        // SHOP Registry
+       this.registry.set('shop_moves', [
+        { key: 'slash', name: 'Slash', description: 'Deals damage to enemy', quantity: 1, cost:100, isSelected: false },
+        { key: 'headbutt', name: 'Headbutt', description: 'Deals damage to enemy', quantity: 1, cost:100, isSelected: false },
+        { key: 'chomp', name: 'Chomp', description: 'Deals damage to enemy', quantity: 1, cost:100, isSelected: false },
+        { key: 'tackle', name: 'Tackle', description: 'Deals damage to enemy', quantity: 1, cost:100, isSelected: false },
+        { key: 'strike', name: 'Strike', description: 'Deals damage to enemy', quantity: 1, cost:100, isSelected: false },
+        { key: 'fireball', name: 'Fireball', description: 'Deals Fire damage to enemy', quantity: 1, cost:150, isSelected: false },
+        { key: 'hydrojet', name: 'Hydrojet', description: 'Deals Water damage to enemy', quantity: 1, cost:150, isSelected: false },
+        { key: 'seismictoss', name: 'Seismic Toss', description: 'Deals Rock damage to enemy', quantity: 1, cost:150, isSelected: false },
+        { key: 'lightjolt', name: 'Lightning Jolt', description: 'Deals Electric damage to enemy', quantity: 1, cost:150, isSelected: false },
+        { key: 'gravpulse', name: 'Gravitic Pulse', description: 'Deals Gravity damage to enemy', quantity: 1, cost:150, isSelected: false }
+        ]); 
+       
+        this.registry.set('shop_astrobeasts', [
+        { key: 'skol', name: 'Skol', description: 'Fire AstroBeast', quantity: 1, cost: 350, isSelected: false },
+        { key: 'tarkeel', name: 'Tarkeel', description: 'Electric AstroBeast', quantity: 1, cost: 350, isSelected: false },
+        { key: 'arquam', name: 'Arquam', description: 'Water AstroBeast', quantity: 1, cost: 350, isSelected: false },
+        { key: 'radrok', name: 'Radrok', description: 'Rock AstroBeast', quantity: 1, cost: 350, isSelected: false },
+        { key: 'zallo', name: 'Zallo', description: 'Gravity AstroBeast', quantity: 1, cost: 350, isSelected: false }
+        ]);
 
-       this.registry.set('shop_astrobeasts', [{ key: 'skol', name: 'Skol', description: 'Fire AstroBeast', quantity: 1, cost: 50, isEquipped: false },
-       { key: 'tarkeel', name: 'Tarkeel', description: 'Water AstroBeast', quantity: 1, cost: 51, isSelected: false }]); // similar as above. showing starter astrobeasts
-
-       this.registry.set('shop_items', [{ key: 'atk_potion', name: 'ATK Potion', description: 'Increases ATK by 10', quantity: 1, cost: 15, isEquipped: false },
-       { key: 'cookies', name: 'CosmoCookies', description: 'Restores 20 HP', quantity: 1, cost: 10, isSelected: false }]); // should be list of dicts with the item name and its key that cooresponds to it's png pat
+        this.registry.set('shop_items', [
+        { key: 'cookies', name: 'CosmoCookies', description: 'Restores 15 HP', quantity: 1, cost: 20, isSelected: false },
+        { key: 'atk_potion', name: 'AstroAde', description: 'Restores 30 HP', quantity: 1, cost: 35, isSelected: false },
+        { key: 'sequid', name: 'SequidSando', description: 'Restores 60 HP', quantity: 1, cost: 60, isSelected: false },
+        { key: 'claws', name: 'Titanium Claws', description: '+10 ATK for the fight duration', quantity: 1, cost: 60, isSelected: false },
+        { key: 'boosters', name: 'Sonic Boosters', description: '+10 SPD for fight duration', quantity: 1, cost: 60, isSelected: false },
+        { key: 'shell', name: 'Hardlight Shell', description: '+10 DEF for fight duration', quantity: 1, cost: 60, isSelected: false },
+        { key: 'stim', name: 'Stim Beacon', description: '+10 DEX, LUK for fight duration', quantity: 1, cost: 60, isSelected: false },
+        { key: 'shield', name: 'Photon Shield', description: '+10 DEX, LUK for fight duration', quantity: 1, cost: 60, isSelected: false },
+        { key: 'gravnet', name: 'Grav Net', description: '+10 DEX, LUK for fight duration', quantity: 1, cost: 80, isSelected: false }
+        ]); 
        
        
        // caps for equipped items/astrobeasts/moves
