d772750e6fe7c190a1990d05fb3c22435f12d103
AJ Boyd <alanjaredboyd3@yahoo.com>
2024-04-18 11:41:34 -0400

    added player object file

---
+++ astrobeasts/static/scripts/start_menu/player.js
@@ -0,0 +1,24 @@
+/*
+class for the Player
+*/
+import { Aliens } from "../combat/aliens.js";
+export class Player{
+    constructor(name, party, cred, xp, maxXP, lvl, battlesWon, s){
+        /**@public @type {string} */
+        this.name = name;
+        /**@public @type {Aliens[]} */
+        this.party = party;
+        /**@public @type {number} */
+        this.credits = cred;
+        /**@public @type {number} */
+        this.currentExp = xp;
+        /**@public @type {number} */
+        this.maxExp = maxXP;
+        /**@public @type {number} */
+        this.level = lvl;
+        /**@public @type {number} */
+        this.battlesWon = battlesWon;
+        /**@public @type {number} */
+        this.score = s; //a sum of exp earned and credits earned
+    }
+}
